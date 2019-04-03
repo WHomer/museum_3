fa6c420077d2d39b6d4acf4b1bf9685f6ab1515c
William Homer <billyhomeriii@gmail.com>
2019-04-03 14:25:23 -0600

    Added Patron class

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -1,2 +1,32 @@
 require 'minitest/autorun'
-require './lib/exhibit'
+require './lib/patron'
+
+class PatronTest < MiniTest::Test
+  def setup
+    @bob = Patron.new('Bob', 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Patron, @bob
+  end
+
+  def test_it_has_a_name
+    assert_equal "Bob", @bob.name
+  end
+
+  def test_it_has_money
+    assert_equal 20, @bob.spending_money
+  end
+
+  def test_it_has_empty_array_of_interests
+    assert_equal [], @bob.interests
+  end
+
+  def test_it_can_add_interests
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+    expected = ["Dead Sea Scrolls", "Gems and Minerals"]
+
+    assert_equal expected, @bob.interests
+  end
+end
